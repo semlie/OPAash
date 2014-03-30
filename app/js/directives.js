1a301f12b8abfc4ecfc35736dbe0cbd3d130923f
Dell <Dell@Dell-PC>
2014-03-30 11:59:34 +0300

    add partials

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,14 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }]).directive('mainMenu', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/top-nev-bar.html'
+    };
+  }).directive('mainBody', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/main.html'
+    };
+  });
